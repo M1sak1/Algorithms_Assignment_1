d17df7defc081d962f9ed6dbecd1c29ea92b45c2
Joshua Corrigan <c3349863@uon.edu.au>
2022-09-20 14:53:16 +1000

    Created and worked on the dfs file also created an object for it

--- DFS.java
+++ DFS.java
@@ -1,3 +1,52 @@
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
 public class DFS {
+    public static void main(String[] args) {
+        String Maze = "";
+        try {
+            File MazeFile = new File("Maze.txt");
+            Scanner myReader = new Scanner(MazeFile);
+            while (myReader.hasNextLine()){
+                Maze += myReader.nextLine();
+            }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        DFSObject DFSHolder = new DFSObject();
+        MazeInformation(DFSHolder, Maze);
+    }
 
+    public static void MazeInformation(DFSObject DFSHolder, String MazeFile){
+        int whichvalue = 0;
+        String output = "";
+        for(int i = 0; i < MazeFile.length();i++){
+            if(Character.compare(MazeFile.charAt(i),':') == 0){
+                if(whichvalue == 0){
+                    DFSHolder.setNumRows(Integer.parseInt(output.substring(0,output.indexOf(','))));
+                    DFSHolder.setNumColumns(Integer.parseInt(output.substring(output.indexOf(','))));
+                    System.out.println(output);
+                    System.out.println(DFSHolder.getNumRows());
+                    System.out.println(DFSHolder.getNumColumns());
+                }
+                if(whichvalue == 1){
+                    System.out.println(output);
+                }
+                if(whichvalue == 2){
+                    System.out.println(output);
+                }
+                whichvalue++;
+                output = "";
+            }
+            else{
+                output += MazeFile.charAt(i);
+            }
+        }
+        System.out.println(output);
+    }
+    public static DFSObject DFS(DFSObject DFSHolder){
+        return DFSHolder;
+    }
 }
---
+++ DFSObject.java
@@ -0,0 +1,75 @@
+public class DFSObject {
+    String Maze = "";
+    int location = -1;
+    int preLocation = -1;
+    int steps = 0;
+    String Path = "";
+    int endPosition = -1;
+    int numRows = 0;
+    int numColumns = 0;
+
+
+    public String getMaze() {
+        return Maze;
+    }
+
+    public void setMaze(String maze) {
+        Maze = maze;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public int getPreLocation() {
+        return preLocation;
+    }
+
+    public void setPreLocation(int preLocation) {
+        this.preLocation = preLocation;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public String getPath() {
+        return Path;
+    }
+
+    public void setPath(String path) {
+        Path = path;
+    }
+
+    public int getEndPosition() {
+        return endPosition;
+    }
+
+    public void setEndPosition(int endPosition) {
+        this.endPosition = endPosition;
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    public void setNumRows(int numRows) {
+        this.numRows = numRows;
+    }
+
+    public int getNumColumns() {
+        return numColumns;
+    }
+
+    public void setNumColumns(int numColumns) {
+        this.numColumns = numColumns;
+    }
+}
