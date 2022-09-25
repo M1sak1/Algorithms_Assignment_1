a708c9343638f7056a3f70a30af04ed5e744dafb
Joshua Corrigan <c3349863@uon.edu.au>
2022-09-25 16:14:39 +1000

    comments

--- DFS.java
+++ DFS.java
@@ -59,7 +59,8 @@ public class DFS {
         DFSHolder.setMaze(output);
         return DFSHolder;
     }
-    public static DFSObject DFS(DFSObject DFSHolder){
+    //tried to do the DFS based on openness but it just cant be done as openess does not provide enough information
+    /*public static DFSObject DFS(DFSObject DFSHolder){
         System.out.println("Location " + DFSHolder.getLocation());
         int currentPrelocation = DFSHolder.getPreLocation();
         int currentLoc = DFSHolder.getLocation();
@@ -275,16 +276,21 @@ public class DFS {
         return DFSHolder;
     }
 
+     */
+//will recurse through this function moving in a possible direction until the end location is found, the path and steps taken are recorded and printed
     static DFSObject DFS2(DFSObject DFSHolder) {
-        //System.out.println(Arrays.deepToString(maze) + "\n" + Arrays.toString(start) + "\n" + Arrays.toString(finish));
         int i;
+        //a current prelocation and location is used to keep track of where this instance is as when a recursion ends its information is passed to the recursive instace that called it, including its locational data
         int currentPrelocation = DFSHolder.getPreLocation();
         int currentLoc = DFSHolder.getLocation();
-        char walls = DFSHolder.getMaze().charAt(DFSHolder.getLocation() - 1); // char list is from 0 and location goes from 1 and getmaze is a string of the walls
+        char walls = DFSHolder.getMaze().charAt(DFSHolder.getLocation() - 1); // char list is from 0 and location goes from 1 and getmaze is a string of all openess. so it will get the openness of our current location
+        //when we find the end position
         if (DFSHolder.getLocation() == DFSHolder.getEndPosition()) {
-            System.out.println("We got here from finishing it");
+            //solve the path so every function instance currently on hold will be given this information and know to put its location into the path
             DFSHolder.setPathSolved(true);
+            //sets our current location as the end of the path
             DFSHolder.setPath(DFSHolder.getPath()  + DFSHolder.getLocation());
+            //increments the tracker of steps taken in path
             DFSHolder.setStepsForPath(DFSHolder.getStepsForPath() + 1);
             return DFSHolder;
         }
@@ -293,10 +299,13 @@ public class DFS {
             if (isPossible(DFSHolder, i)) {
                 switch (i) {
                     case 0 -> { //right
-                        DFSHolder.setPreLocation(currentLoc);
+                        DFSHolder.setPreLocation(currentLoc); //sets up new location and pre-location variables for the next node
                         DFSHolder.setLocation(currentLoc + 1);
+                        //holds the actual steps taken to find the solution
                         DFSHolder.setSteps(DFSHolder.getSteps() + 1);
+                        //recurses,
                         DFS2(DFSHolder);
+                        //breaks the for if the end is found
                         if (DFSHolder.isPathSolved()) {
                             i = 10; //break the loop
                         }
@@ -330,21 +339,26 @@ public class DFS {
                         }
                     }
                 }
+                //sets the locations back to what their meant to be, before trying a new path
                 DFSHolder.setPreLocation(currentPrelocation);
                 DFSHolder.setLocation(currentLoc);
             }
         }
+        //when the end is found all recursive instances that were used to get to that path will end and they will have held the true pathsolved attribute from the finish node and know to add their location to the path
         if (DFSHolder.isPathSolved()){
             DFSHolder.setPath(currentLoc +","+ DFSHolder.getPath());
             DFSHolder.setStepsForPath(DFSHolder.getStepsForPath() + 1);
         }
         return DFSHolder;
     }
+    //a function used to determine if a direction to travel is possible based on the maze's size and openness of this location and surrounding locations
     static boolean isPossible(DFSObject DFSHolder, int dir){
         switch(dir) {
             //Right
             case 0:
+                //checks if the previous location is where we are trying to go and if this move would wrap use onto the next column down from going right
                 if (DFSHolder.getPreLocation() != DFSHolder.getLocation() + 1 && DFSHolder.getLocation() % DFSHolder.getNumColumns() != 0 ) {
+                    //checks the openness of our current location and makes sure its 1 or 3
                     if (Character.getNumericValue(DFSHolder.getMaze().charAt(DFSHolder.getLocation() - 1)) % 2 != 0) {
                         return true;
                     }
@@ -352,7 +366,9 @@ public class DFS {
                 break;
             //Down
             case 1:
+                ////checks if the previous location is where we are trying to go and if moving down would put us through the maze's bottom
                 if (DFSHolder.getPreLocation() != DFSHolder.getLocation() + DFSHolder.getNumColumns() && DFSHolder.getLocation() + DFSHolder.getNumColumns() <= (DFSHolder.getNumColumns() * DFSHolder.getNumRows())){
+                    //checks openness of this location and if its 2,3 you can move down
                     if (Character.getNumericValue(DFSHolder.getMaze().charAt((DFSHolder.getLocation() - 1)))  > 1) {
                         return true;
                     }
@@ -362,6 +378,7 @@ public class DFS {
             case 2:
                 // check if the move is possible
                 if((DFSHolder.getLocation() - 1) % DFSHolder.getNumColumns() != 0 && DFSHolder.getLocation() - 1 != DFSHolder.getPreLocation()){
+                    //will check the openness of the location to the left (char starts at 0 and location from 1) ensuring the left openness is 1,3
                     if (Character.getNumericValue(DFSHolder.getMaze().charAt(DFSHolder.getLocation() - 2)) % 2 != 0) {
                         return true;
                     }
@@ -371,6 +388,7 @@ public class DFS {
             case 3:
                 //checking if the move is possible
                 if(DFSHolder.getLocation() - DFSHolder.getNumColumns() > 0 && DFSHolder.getLocation() - DFSHolder.getNumColumns() != DFSHolder.getPreLocation()){
+                    //same thing as case 1 but checks the openness of the location above as it needs to be 2,3
                     if (Character.getNumericValue(DFSHolder.getMaze().charAt((DFSHolder.getLocation() - DFSHolder.getNumColumns()) - 1)) > 1){
                         return true;
                     }
@@ -380,57 +398,74 @@ public class DFS {
         // returns false by default
         return false;
     }
-
+//this function takes the completed DFSHolder with all openness, path, start, finish and prints a maze with a path with it
     public static void PrinttextMaze(DFSObject DFSHolder){
         //constructing the walls
         String onLine = "-";
         String betweenLine = "|";
+        //our location to go through the entire maze
         int location = 1;
+        //the openness of this node
         int direction;
+        //changing the stored path example. x,xx,x,xx,xxx into a useable array of values
         int[] storePath = new int[DFSHolder.getStepsForPath()];
         int q = 0;
         int p = 0;
-        String holder = "";
+        String holder = ""; //holds the current values of a path location
+        //goes through the string and takes the path location values and puts them in a int array, sectioned by the ,'s in the path string
         while(p < DFSHolder.getPath().length()){
             if(DFSHolder.getPath().charAt(p) == ','){
                 storePath[q] = Integer.parseInt(holder);
+                //increments the storing array
                 q++;
                 holder = "";
             }
             else {
+                //adds to the current string
                 holder += DFSHolder.getPath().charAt(p);
             }
             p++;
         }
+        //puts the last value in
         storePath[q] = Integer.parseInt(holder);
 
+        //used to return 2 values from a function
         String[] returnHolder;
         for(int i = 0 ; i < DFSHolder.getNumColumns(); i++){
             onLine += "---";
         }
+        //prints the top wall
         System.out.println(onLine);
+
         for(int i = 0; i < DFSHolder.getNumRows(); i++){
             onLine = "|";
             betweenLine = "|";
             for(int j = 0; j < DFSHolder.getNumColumns(); j++){
+                //grabs openness of current location
                 direction = Character.getNumericValue(DFSHolder.getMaze().charAt(location - 1));
+                //generates the whitespace or walls for the lines and checks if we are on the path
                 returnHolder = PrintWalls(direction,onLine,betweenLine,storePath, location);
+                //saving values
                 onLine = returnHolder[0];
                 betweenLine = returnHolder[1];
+                //increment our tracker
                 location++;
             }
+            //prints our online
             System.out.println(onLine);
+            //prints our between line if were not at the end of the maze
             if(i + 1 != DFSHolder.getNumRows()){
                 System.out.println(betweenLine);
             }
         }
+        //prints the bottom wall
         onLine = "-";
         for(int i = 0 ; i < DFSHolder.getNumColumns(); i++){
             onLine += "---";
         }
         System.out.println(onLine);
     }
-
+    //adds the required characters when printing the completed maze
     public static String[] PrintWalls(int direction, String onLine, String betweenLine, int[] path, int location){
         String[] Lines = new String[]{onLine,betweenLine};
         boolean start = false;
@@ -438,6 +473,7 @@ public class DFS {
         boolean onPath = false;
         //[0] is online
         //[1] is betweenLines
+        //checks if the current location is on the path(including the start and finish
         for(int p = 0; p < path.length; p++){
             if(path[p] == location){
                 if(path[0] == location){
@@ -450,6 +486,7 @@ public class DFS {
                 break;
             }
         }
+        //creates the walls and necessary path values based on this locations openness
         switch(direction){
             case 0:{
                 //will create one below and to the right
